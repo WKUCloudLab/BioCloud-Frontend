d335abd952f6a13713ead7000e8b6c7abed511e2
Jamie Thomas <31860384+JamieThomas-WKU@users.noreply.github.com>
2018-09-16 23:56:50 -0500

    Added in one command for the bowtie2 biotool.

--- webapp/testJSON.js
+++ webapp/testJSON.js
@@ -101,5 +101,33 @@ module.exports = {
         }
       ],
     },
+    {
+      "name": "Bowtie2",
+      "description": "An ultrafast and memory-efficient tool for aligning sequencing reads to long reference sequences. It is particularly good at aligning reads of about 50 up to 100s or 1,000s of characters, and particularly good at aligning to relatively long (e.g. mammalian) genomes. Bowtie 2 indexes the genome with an FM Index to keep its memory footprint small: for the human genome, its memory footprint is typically around 3.2 GB. Bowtie 2 supports gapped, local, and paired-end alignment modes.",
+      "id": "bowtie2",
+      "commands":[
+        {
+          "name": null,
+          "id": "fastqc",
+          "description": null,
+          "args":{
+            "required":[],
+            "optional":[
+              {
+                "name": "Bowtie 2 Aligner",
+                "id": "aligner",
+                "description": "Aligns a set of unpaired reads to the Lambda phage reference genome using an index. The alignment results in SAM format are written to the file result.sam.",
+                "inputType": "boolean",
+                "required": false,
+                "place-holder": null,
+                "dependencies": null,
+                "options": null,
+              },
+            ],
+          },
+          "conflicting-args":[],
+        }
+      ],
+    },
   ]
 };
